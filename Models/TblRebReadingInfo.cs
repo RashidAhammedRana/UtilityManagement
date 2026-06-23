b336d62428fcdce06e8d9d66417d06816023b011
Rashid Ahammed Rana <ahammed.rashid94@gmail.com>
2026-06-23 22:13:22 +0600

    Updated

--- Models/TblRebReadingInfo.cs
+++ Models/TblRebReadingInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using UtilityManagement.Models;
 
 namespace UtilityManagement.Models;
@@ -9,25 +10,25 @@ public partial class TblRebReadingInfo
     public int Trid { get; set; }
 
     public DateTime? Trdate { get; set; }
-
+    [Required(ErrorMessage = "This field is required")]
     public int? Eqid { get; set; }
-
+    [Required(ErrorMessage = "This field is required")]
     public double? ElecGen { get; set; }
-
+    [Required(ErrorMessage = "This field is required")]
     public double? RunHr { get; set; }
-
+    [Required(ErrorMessage = "This field is required")]
     public double? BdtKwh { get; set; }
-
+    [Required(ErrorMessage = "This field is required")]
     public double? OtConsumable { get; set; }
-
+    [Required(ErrorMessage = "This field is required")]
     public double? Troubleshoot { get; set; }
-
+    [Required(ErrorMessage = "This field is required")]
     public double? ServiceCharge { get; set; }
-
+    [Required(ErrorMessage = "This field is required")]
     public double? RepairCharge { get; set; }
-
+    [Required(ErrorMessage = "This field is required")]
     public double? Total { get; set; }
-
+    [Required(ErrorMessage = "This field is required")]
     public double? TkKwh { get; set; }
 
     public string? Remarks { get; set; }

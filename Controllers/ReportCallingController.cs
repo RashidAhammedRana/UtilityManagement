ac6cc20e89279afed8faf7b33d0611ca8a7e6be4
Rashid Ahammed Rana <ahammed.rashid94@gmail.com>
2026-07-30 13:21:02 +0600

    update NG Gen. Cost Report

--- Controllers/ReportCallingController.cs
+++ Controllers/ReportCallingController.cs
@@ -1,24 +1,4 @@
-﻿//using DevExpress.XtraReports.UI;
-//using Microsoft.AspNetCore.Mvc;
-
-//public class ReportCallingController : Controller
-//{
-//    public IActionResult RebCostReport(string reportName = "rptShiftWiseCounterMetrics")
-//    {
-//        try
-//        {
-//            var rptPath = $"UtilityManagement.Reports.{reportName}";
-//            XtraReport report = (XtraReport)Activator.CreateInstance(Type.GetType(rptPath));
-//            return View(report);
-//        }
-//        catch (Exception ex)
-//        {
-//            throw ex.InnerException;
-//        }
-//    }
-//}
-
-using DevExpress.XtraReports.UI;
+﻿using DevExpress.XtraReports.UI;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
@@ -33,35 +13,67 @@ public class ReportCallingController : Controller
         _context = context;
     }
 
-    public async Task<IActionResult> RebCostReport(
-        int menuId,
-        string reportName = "rptRebCost")
+
+    private async Task<bool> HasViewPermission(int menuId)
     {
-        // Get logged-in user
         var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-        // Get user permissions
-        var userPermissions = await (
+        return await (
             from up in _context.TblUserPermission
             join pa in _context.TblPermissionAction
                 on up.ActionId equals pa.ActionId
             where up.UserId == userId
                   && up.MenuId == menuId
                   && up.IsAllowed
-            select pa.ActionName
-        ).ToListAsync();
+                  && pa.ActionName == "View"
+            select up
+        ).AnyAsync();
+    }
 
-        ViewBag.CanView = userPermissions.Contains("View");
 
-        // Check View permission
-        if (!ViewBag.CanView)
+    public async Task<IActionResult> RebCostReport(
+        int menuId,
+        string reportName = "rptRebCost")
+    {
+        if (!await HasViewPermission(menuId))
         {
-            return Forbid(); // or Unauthorized();
+            return Forbid();
         }
 
         try
         {
             var rptPath = $"UtilityManagement.Reports.{reportName}";
+
+            var reportType = Type.GetType(rptPath);
+
+            if (reportType == null)
+            {
+                return NotFound($"Report '{reportName}' not found.");
+            }
+
+            var report = (XtraReport)Activator.CreateInstance(reportType);
+
+            return View(report);
+        }
+        catch (Exception ex)
+        {
+            return Content(ex.InnerException?.Message ?? ex.Message);
+        }
+    }
+
+    public async Task<IActionResult> NgGeneratorCostReport(
+    int menuId,
+    string reportName = "rptNgGeneratorCost")
+    {
+        if (!await HasViewPermission(menuId))
+        {
+            return Forbid();
+        }
+
+        try
+        {
+            var rptPath = $"UtilityManagement.Reports.{reportName}";
+
             var reportType = Type.GetType(rptPath);
 
             if (reportType == null)
